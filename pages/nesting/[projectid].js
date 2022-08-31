9923400d221ccf556573e21cb2e941552260c240
taesubyun <taesubyun@icloud.com>
2022-09-01 00:23:59 +0900

    Extracting Dynamic Route Segment Data (Dynamic Routes)

--- pages/nesting/[projectid].js
+++ pages/nesting/[projectid].js
@@ -1,4 +1,10 @@
+import { useRouter } from 'next/router';
+
 function NestingProjectPage() {
+  const router = useRouter();
+  console.log(router.pathname);
+  console.log(router.query);
+
   return (
     <div>
       <h1>The NestingProjectPage</h1>
